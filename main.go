cd85146f852a1d923677b264d85817085482af99
Nilesh Dalagade <79205797+nsd8888@users.noreply.github.com>
2023-04-20 13:13:31 +0530

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	//st := time.Now()
 	//tmp := template.Must(template.ParseFiles("/views/home.html"))
 	mux := Controller.Serving_func()
-	http.ListenAndServe("localhost:8880", mux)
+	http.ListenAndServe("0.0.0.0", mux)
 
 	//Database.Db.Create(&city_info_structs{City: "dudhani", Population: 400000})
 
