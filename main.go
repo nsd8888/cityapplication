23b773efea34584860d6d991cec0b8437a6bc0ba
Nilesh Dalagade <79205797+nsd8888@users.noreply.github.com>
2023-04-20 19:01:48 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ func init() {
 	Database.Getconnection()
 	Database.Migratedatabase()
 }
-}
+
 
 func main() {
 	//st := time.Now()
 	//tmp := template.Must(template.ParseFiles("/views/home.html"))
 	mux := Controller.Serving_func()
-	http.ListenAndServe("0.0.0.0:8820", mux)
+	http.ListenAndServe("0.0.0.0:8850", mux)
 
 	//Database.Db.Create(&city_info_structs{City: "dudhani", Population: 400000})
 
