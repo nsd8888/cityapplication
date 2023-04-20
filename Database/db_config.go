cededff228da3de0e83d228990d4158c7b8e532d
Nilesh Dalagade <79205797+nsd8888@users.noreply.github.com>
2023-04-20 09:53:42 +0530

    Update db_config.go

--- Database/db_config.go
+++ Database/db_config.go
@@ -11,7 +11,7 @@ import (
 var Db *gorm.DB
 
 func Getconnection() {
-	connection := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", "root", "Blucognition@88", "localhost", "new_project")
+	connection := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", "admin", "Hemantnilesh88", "database-1.cetjcletfp74.us-east-1.rds.amazonaws.com", "database-1")
 	db, err := gorm.Open(mysql.New(mysql.Config{DSN: connection}), &gorm.Config{})
 	if err != nil {
 		log.Println("DB connection Failed")
